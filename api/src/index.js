0464d7e67725fc0bf66170f475df3397c4c7936d
Alexandr Timoshenskiy <timoshenskiyad@pik.ru>
2022-06-18 01:18:54 +0300

    add volumes

--- api/src/index.js
+++ api/src/index.js
@@ -2,12 +2,26 @@ const express = require('express')
 const { connectDb } = require('./helpers/db')
 const { port, host, db } = require('./configuration')
 const app = express();
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    name: String
+})
+
+const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
     app.listen(port, () => {
         console.log(`Started api service on port: ${port}`);
         console.log(`Started api service on host: ${host}`);
         console.log(`Our database: ${db}`);
+
+        const silence = new Post({ name: "Silence"})
+
+        silence.save((err, savedSilence)=>{
+            if (err) return console.error(err);
+            console.log("saved silence with volumes", savedSilence);
+        })
     })
 }
 
